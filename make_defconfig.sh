MotorolaMobilityLLC/kernel-msm

Add make_defconfig for easier update of kernel defconfigs
Change-Id: I2f8a7e0f7107db8279e1cf95f68404f34e35467a

make_defconfig.sh
@@ -1,0 +1,27 @@
+#!/bin/bash
+
+DEFCONFIG_FILE=$1
+if [ -z "$DEFCONFIG_FILE" ]; then
+	echo "Need defconfig file(seed_defconfig)!"
+	exit -1
+fi
+
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+# make .config
+env KCONFIG_NOTIMESTAMP=true \
+make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE}
+
+# run menuconfig
+env KCONFIG_NOTIMESTAMP=true \
+make menuconfig ARCH=arm
+make savedefconfig ARCH=arm
+
+# copy .config to defconfig
+mv defconfig arch/arm/configs/${DEFCONFIG_FILE}
+
+# clean kernel object
+make mrproper

jmpfbmx/android_kernel_bq_msm8937-common

make_defconfig.sh
@@ -1,0 +1,27 @@
+#!/bin/bash
+
+DEFCONFIG_FILE=$1
+if [ -z "$DEFCONFIG_FILE" ]; then
+	echo "Need defconfig file(msm_defconfig)!"
+	exit -1
+fi
+
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+# make .config
+env KCONFIG_NOTIMESTAMP=true \
+make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE}
+
+# run menuconfig
+env KCONFIG_NOTIMESTAMP=true \
+make menuconfig ARCH=arm
+make savedefconfig ARCH=arm
+
+# copy .config to defconfig
+mv defconfig arch/arm/configs/${DEFCONFIG_FILE}
+
+# clean kernel object
+make mrproper